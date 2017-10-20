682f2577c8cf94262b7e7ea7bb5e217e4804601e
Nikos <nikotheract11@gmail.com>
2017-10-20 15:33:36 +0300

    add some stuff

---
+++ heap_file_64/src/heap_file.c
@@ -0,0 +1,43 @@
+#include "heap_file.h"
+
+HP_ErrorCode HP_Init() {
+  // write your code here
+  
+  return HP_OK;
+}
+
+HP_ErrorCode HP_CreateIndex(const char *filename) {
+  // write your code here
+
+  return HP_OK;
+}
+
+HP_ErrorCode HP_OpenFile(const char *fileName, int *fileDesc){
+  // write your code here
+
+  return HP_OK;
+}
+
+HP_ErrorCode HP_CloseFile(int fileDesc) {
+  // write your code here
+
+  return HP_OK;
+}
+
+HP_ErrorCode HP_InsertEntry(int fileDesc, Record record) {
+  // write your code here
+
+  return HP_OK;
+}
+
+HP_ErrorCode HP_PrintAllEntries(int fileDesc) {
+  // write your code here
+
+  return HP_OK;
+}
+
+HP_ErrorCode HP_GetEntry(int fileDesc, int rowId, Record *record) {
+  // write your code here
+  
+  return HP_OK;
+}
